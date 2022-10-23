e75fdf2737d87c5bb1e499bd537f4bc71e5b681f
dogustuluk <dogus.tuluk@gmail.com>
2022-10-23 17:47:05 +0300

    created Banka entity migration configuration

--- src/OnMuhasebe.BlazorProject.EntityFrameworkCore/EntityFrameworkCore/BlazorProjectDbContext.cs
+++ src/OnMuhasebe.BlazorProject.EntityFrameworkCore/EntityFrameworkCore/BlazorProjectDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.Data;
+using Microsoft.EntityFrameworkCore;
 using OnMuhasebe.BlazorProject.BankaHesaplar;
 using OnMuhasebe.BlazorProject.Bankalar;
 using OnMuhasebe.BlazorProject.BankaSubeler;
 using OnMuhasebe.BlazorProject.Birimler;
 using OnMuhasebe.BlazorProject.Cariler;
+using OnMuhasebe.BlazorProject.Consts;
 using OnMuhasebe.BlazorProject.Depolar;
 using OnMuhasebe.BlazorProject.Donemler;
 using OnMuhasebe.BlazorProject.Faturalar;
@@ -20,6 +22,7 @@ using Volo.Abp.BackgroundJobs.EntityFrameworkCore;
 using Volo.Abp.Data;
 using Volo.Abp.DependencyInjection;
 using Volo.Abp.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore.Modeling;
 using Volo.Abp.FeatureManagement.EntityFrameworkCore;
 using Volo.Abp.Identity;
 using Volo.Abp.Identity.EntityFrameworkCore;
@@ -108,11 +111,45 @@ public class BlazorProjectDbContext :
 
         /* Configure your own tables/entities inside here */
 
-        //builder.Entity<YourEntity>(b =>
-        //{
-        //    b.ToTable(BlazorProjectConsts.DbTablePrefix + "YourEntities", BlazorProjectConsts.DbSchema);
-        //    b.ConfigureByConvention(); //auto configure for the base class props
-        //    //...
-        //});
+        builder.Entity<Banka>(b =>
+        {
+            b.ToTable(BlazorProjectConsts.DbTablePrefix + "Bankalar", BlazorProjectConsts.DbSchema);//blazorProjectConst ile oluşturulan tablonun önünde hangi string değerin geleceğini belirtir. burada App olarak verilmiş, isteğe bağlı olarak o sınıftan bu isim değiştirilebilir.
+            b.ConfigureByConvention(); //auto configure for the base class props
+            //properties->
+            b.Property(x => x.Kod)
+                .IsRequired()
+                .HasColumnType(SqlDbType.VarChar.ToString())
+                .HasMaxLength(EntityConsts.MaxKodLength);
+
+            b.Property(x => x.Ad)
+                .IsRequired()
+                .HasColumnType(SqlDbType.VarChar.ToString())
+                .HasMaxLength(EntityConsts.MaxAdLength);
+            
+            b.Property(x => x.OzelKod1)
+                .HasColumnType(SqlDbType.UniqueIdentifier.ToString());
+            
+            b.Property(x => x.OzelKod2)
+                .HasColumnType(SqlDbType.UniqueIdentifier.ToString());
+            
+            b.Property(x => x.Aciklama)
+                .HasColumnType(SqlDbType.VarChar.ToString())
+                .HasMaxLength(EntityConsts.MaxAciklamaLength);
+            
+            b.Property(x => x.Durum)
+                .HasColumnType(SqlDbType.Bit.ToString());
+            //indexes->
+            b.HasIndex(x => x.Kod);//kod alanına göre sıralama hem de arama yaptırabiliriz. Dolayısıyla herhangi bir tipe göre benzeri işlemler yapıyorsak index'lememiz gerekmektedir performans açısından.
+            //relations->
+            b.HasOne(x => x.OzelKod1)
+            .WithMany(x => x.OzelKod1Bankalar)
+            .OnDelete(DeleteBehavior.NoAction);//noAction ->eğer banka entity'si silinirse ozelKod1'i silmesin. daha sonra aynı banka tekrar tanımlandığı zaman orada tanımlanmış olan(ozelKodlar'da tanımlanmış olan) ozelKodu kullanabilsin diye.
+            
+            b.HasOne(x => x.OzelKod2)
+            .WithMany(x => x.OzelKod2Bankalar)
+            .OnDelete(DeleteBehavior.NoAction);
+
+
+        });
     }
 }
