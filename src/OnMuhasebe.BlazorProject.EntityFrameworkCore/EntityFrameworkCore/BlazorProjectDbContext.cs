11ed582d42fd04656df589a829adc0c66bb4303e
dogustuluk <dogus.tuluk@gmail.com>
2022-10-23 17:51:44 +0300

    created BlazorProjectDbContextModelBuilderExtensions

--- src/OnMuhasebe.BlazorProject.EntityFrameworkCore/EntityFrameworkCore/BlazorProjectDbContext.cs
+++ src/OnMuhasebe.BlazorProject.EntityFrameworkCore/EntityFrameworkCore/BlazorProjectDbContext.cs
@@ -5,6 +5,7 @@ using OnMuhasebe.BlazorProject.Bankalar;
 using OnMuhasebe.BlazorProject.BankaSubeler;
 using OnMuhasebe.BlazorProject.Birimler;
 using OnMuhasebe.BlazorProject.Cariler;
+using OnMuhasebe.BlazorProject.Configurations;
 using OnMuhasebe.BlazorProject.Consts;
 using OnMuhasebe.BlazorProject.Depolar;
 using OnMuhasebe.BlazorProject.Donemler;
@@ -110,46 +111,10 @@ public class BlazorProjectDbContext :
         builder.ConfigureTenantManagement();
 
         /* Configure your own tables/entities inside here */
-
-        builder.Entity<Banka>(b =>
-        {
-            b.ToTable(BlazorProjectConsts.DbTablePrefix + "Bankalar", BlazorProjectConsts.DbSchema);//blazorProjectConst ile oluşturulan tablonun önünde hangi string değerin geleceğini belirtir. burada App olarak verilmiş, isteğe bağlı olarak o sınıftan bu isim değiştirilebilir.
-            b.ConfigureByConvention(); //auto configure for the base class props
-            //properties->
-            b.Property(x => x.Kod)
-                .IsRequired()
-                .HasColumnType(SqlDbType.VarChar.ToString())
-                .HasMaxLength(EntityConsts.MaxKodLength);
-
-            b.Property(x => x.Ad)
-                .IsRequired()
-                .HasColumnType(SqlDbType.VarChar.ToString())
-                .HasMaxLength(EntityConsts.MaxAdLength);
-            
-            b.Property(x => x.OzelKod1)
-                .HasColumnType(SqlDbType.UniqueIdentifier.ToString());
-            
-            b.Property(x => x.OzelKod2)
-                .HasColumnType(SqlDbType.UniqueIdentifier.ToString());
-            
-            b.Property(x => x.Aciklama)
-                .HasColumnType(SqlDbType.VarChar.ToString())
-                .HasMaxLength(EntityConsts.MaxAciklamaLength);
-            
-            b.Property(x => x.Durum)
-                .HasColumnType(SqlDbType.Bit.ToString());
-            //indexes->
-            b.HasIndex(x => x.Kod);//kod alanına göre sıralama hem de arama yaptırabiliriz. Dolayısıyla herhangi bir tipe göre benzeri işlemler yapıyorsak index'lememiz gerekmektedir performans açısından.
-            //relations->
-            b.HasOne(x => x.OzelKod1)
-            .WithMany(x => x.OzelKod1Bankalar)
-            .OnDelete(DeleteBehavior.NoAction);//noAction ->eğer banka entity'si silinirse ozelKod1'i silmesin. daha sonra aynı banka tekrar tanımlandığı zaman orada tanımlanmış olan(ozelKodlar'da tanımlanmış olan) ozelKodu kullanabilsin diye.
-            
-            b.HasOne(x => x.OzelKod2)
-            .WithMany(x => x.OzelKod2Bankalar)
-            .OnDelete(DeleteBehavior.NoAction);
+        builder.ConfigureBanka();
 
 
-        });
+
+
     }
 }
