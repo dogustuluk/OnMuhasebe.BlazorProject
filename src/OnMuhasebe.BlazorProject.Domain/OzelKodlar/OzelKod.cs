5353e2968ff0f96d8f11719d6dc84bce82186c35
dogustuluk <dogus.tuluk@gmail.com>
2022-10-23 15:01:30 +0300

    Completed entities and enums creation

---
+++ src/OnMuhasebe.BlazorProject.Domain/OzelKodlar/OzelKod.cs
@@ -0,0 +1,10 @@
+﻿namespace OnMuhasebe.BlazorProject.OzelKodlar;
+public class OzelKod:FullAuditedAggregateRoot<Guid>
+{
+    public string Kod { get; set; }
+    public string Ad { get; set; }
+    public OzelKodTuru KodTuru { get; set; }
+    public KartTuru KartTuru { get; set; }
+    public string Aciklama { get; set; }
+    public bool Durum { get; set; }
+}
---
+++ src/OnMuhasebe.BlazorProject.Domain/Subeler/Sube.cs
@@ -0,0 +1,8 @@
+﻿namespace OnMuhasebe.BlazorProject.Subeler;
+public class Sube:FullAuditedAggregateRoot<Guid>
+{
+    public string Kod { get; set; }
+    public string Ad { get; set; }
+    public string Aciklama { get; set; }
+    public bool Durum { get; set; }
+}
