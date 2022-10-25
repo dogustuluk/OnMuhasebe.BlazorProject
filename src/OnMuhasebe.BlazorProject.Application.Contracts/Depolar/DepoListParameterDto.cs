7ff70dd700c89796cf9f5802bfb6be19bf8294a3
dogustuluk <dogus.tuluk@gmail.com>
2022-10-25 19:06:37 +0300

    Depo dto

---
+++ src/OnMuhasebe.BlazorProject.Application.Contracts/Depolar/DepoListParameterDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using OnMuhasebe.BlazorProject.CommonDtos;
+using Volo.Abp.Application.Dtos;
+
+namespace OnMuhasebe.BlazorProject.Depolar;
+public class DepoListParameterDto : PagedResultRequestDto, IDurum, IEntityDto
+{
+    public Guid SubeId { get; set; }
+    public bool Durum { get; set; }
+}
