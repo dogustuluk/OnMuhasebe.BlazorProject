c0790d87f37e9aa4c8f8d03da7e889b92f03ae28
dogustuluk <dogus.tuluk@gmail.com>
2022-10-25 18:38:44 +0300

    BankaSube dto

---
+++ src/OnMuhasebe.BlazorProject.Application.Contracts/BankaSubeler/BankaSubeListParameterDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OnMuhasebe.BlazorProject.CommonDtos;
+using Volo.Abp.Application.Dtos;
+
+namespace OnMuhasebe.BlazorProject.BankaSubeler;
+public class BankaSubeListParameterDto : PagedResultRequestDto, IDurum, IEntityDto
+{
+    public Guid BankaId { get; set; }
+    public bool Durum { get; set; }
+}
