6405e6f440403178018611df7912f1bc82ac2347
dogustuluk <dogus.tuluk@gmail.com>
2022-10-25 19:10:33 +0300

    Donem dto

---
+++ src/OnMuhasebe.BlazorProject.Application.Contracts/Donemler/UpdateDonemDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Application.Dtos;
+
+namespace OnMuhasebe.BlazorProject.Donemler;
+public class UpdateDonemDto : IEntityDto
+{
+    public string Kod { get; set; }
+    public string Ad { get; set; }
+    public string Aciklama { get; set; }
+    public bool Durum { get; set; }
+}
