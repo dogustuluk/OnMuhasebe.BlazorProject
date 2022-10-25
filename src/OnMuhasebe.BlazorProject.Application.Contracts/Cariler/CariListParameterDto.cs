8e7125634125f0c179ee0ad65af13d6500775fab
dogustuluk <dogus.tuluk@gmail.com>
2022-10-25 18:59:00 +0300

    Cari dto

---
+++ src/OnMuhasebe.BlazorProject.Application.Contracts/Cariler/CariListParameterDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OnMuhasebe.BlazorProject.CommonDtos;
+using Volo.Abp.Application.Dtos;
+
+namespace OnMuhasebe.BlazorProject.Cariler;
+public class CariListParameterDto : PagedResultRequestDto, IDurum, IEntityDto
+{
+    public bool Durum { get; set; }
+}
