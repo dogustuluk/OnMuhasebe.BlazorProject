f2a39a29b207698278ff0d97a1f3b95959681e49
dogustuluk <dogus.tuluk@gmail.com>
2022-10-26 18:54:21 +0300

    FirmaParametre dtos

---
+++ src/OnMuhasebe.BlazorProject.Application.Contracts/Parametreler/FirmaParametreListParameterDto.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Application.Dtos;
+
+namespace OnMuhasebe.BlazorProject.Parametreler;
+public class FirmaParametreListParameterDto : PagedResultRequestDto, IEntityDto
+{
+}
