521a39fc5b17c7e0b1578c831def49275197f9d3
dogustuluk <dogus.tuluk@gmail.com>
2022-10-26 16:59:21 +0300

    Hizmet dtos

---
+++ src/OnMuhasebe.BlazorProject.Application.Contracts/Hizmetler/HizmetListParameterDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OnMuhasebe.BlazorProject.CommonDtos;
+using Volo.Abp.Application.Dtos;
+
+namespace OnMuhasebe.BlazorProject.Hizmetler;
+public class HizmetListParameterDto : PagedResultRequestDto, IDurum, IEntityDto
+{
+    public bool Durum { get; set; }
+}
