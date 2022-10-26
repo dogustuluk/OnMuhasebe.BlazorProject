db72fedfbddb9fd25135d75d415ba1ccd37961e5
dogustuluk <dogus.tuluk@gmail.com>
2022-10-26 17:08:55 +0300

    Kasa dtos

---
+++ src/OnMuhasebe.BlazorProject.Application.Contracts/Kasalar/KasaCodeParameterDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using OnMuhasebe.BlazorProject.CommonDtos;
+using Volo.Abp.Application.Dtos;
+
+namespace OnMuhasebe.BlazorProject.Kasalar;
+public class KasaCodeParameterDto : IDurum, IEntityDto
+{
+    public Guid SubeId { get; set; }
+    public bool Durum { get; set; }
+}
