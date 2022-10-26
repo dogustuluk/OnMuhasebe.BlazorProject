dc4e76f7a44f42876ff4a43eaa52b1d3abcebf1f
dogustuluk <dogus.tuluk@gmail.com>
2022-10-26 18:48:12 +0300

    OzelKod dtos

---
+++ src/OnMuhasebe.BlazorProject.Application.Contracts/OzelKodlar/OzelKodCodeParameterDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OnMuhasebe.BlazorProject.CommonDtos;
+using Volo.Abp.Application.Dtos;
+
+namespace OnMuhasebe.BlazorProject.OzelKodlar;
+public class OzelKodCodeParameterDto : IDurum, IEntityDto
+{
+    public OzelKodTuru KodTuru { get; set; }
+    public KartTuru KartTuru { get; set; }
+    public bool Durum { get; set; }
+}
