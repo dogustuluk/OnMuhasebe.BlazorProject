6f3ab184ea436bb07e3aef0c654efb6959143dff
dogustuluk <dogus.tuluk@gmail.com>
2022-11-06 14:15:59 +0300

    FirmaParametreAppService

--- src/OnMuhasebe.BlazorProject.Application.Contracts/Parametreler/IFirmaParametreAppService.cs
+++ src/OnMuhasebe.BlazorProject.Application.Contracts/Parametreler/IFirmaParametreAppService.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using OnMuhasebe.BlazorProject.Services;
 
 namespace OnMuhasebe.BlazorProject.Parametreler;
 public interface IFirmaParametreAppService : ICrudAppService<SelectFirmaParametreDto, SelectFirmaParametreDto, FirmaParametreListParameterDto, CreateFirmaParametreDto, UpdateFirmaParametreDto>
 {
+    Task<bool> UserAnyAsync(Guid userId); 
 }
--- src/OnMuhasebe.BlazorProject.Application/BlazorProjectApplicationAutoMapperProfile.cs
+++ src/OnMuhasebe.BlazorProject.Application/BlazorProjectApplicationAutoMapperProfile.cs
@@ -15,6 +15,7 @@ using OnMuhasebe.BlazorProject.MakbuzHareketler;
 using OnMuhasebe.BlazorProject.Makbuzlar;
 using OnMuhasebe.BlazorProject.Masraflar;
 using OnMuhasebe.BlazorProject.OzelKodlar;
+using OnMuhasebe.BlazorProject.Parametreler;
 
 namespace OnMuhasebe.BlazorProject;
 
@@ -260,6 +261,16 @@ public class BlazorProjectApplicationAutoMapperProfile : Profile
         CreateMap<OzelKod, ListOzelKodDto>();
         CreateMap<CreateOzelKodDto, OzelKod>();
         CreateMap<UpdateOzelKodDto, OzelKod>();
+
+        //FirmaParametre
+        CreateMap<FirmaParametre, SelectFirmaParametreDto>()
+            .ForMember(x => x.SubeAdi, y => y.MapFrom(z => z.Sube.Ad))
+            .ForMember(x => x.DonemAdi, y => y.MapFrom(z => z.Donem.Ad))
+            .ForMember(x => x.DepoAdi, y => y.MapFrom(z => z.Depo.Ad));
+
+        CreateMap<CreateFirmaParametreDto, FirmaParametre>();
+        CreateMap<UpdateFirmaParametreDto, FirmaParametre>();
+
 
     }
 }
