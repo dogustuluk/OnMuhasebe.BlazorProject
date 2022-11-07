c4929d76df7c1948d83c4238a3975cf372c50d06
dogustuluk <dogus.tuluk@gmail.com>
2022-11-07 13:23:00 +0300

    Banka validator

---
+++ src/OnMuhasebe.BlazorProject.Application.Contracts/Usings.cs
@@ -0,0 +1,7 @@
+﻿global using FluentValidation;
+global using Microsoft.Extensions.Localization;
+global using OnMuhasebe.BlazorProject.Consts;
+global using OnMuhasebe.BlazorProject.Localization;
+global using System;
+global using System.Collections.Generic;
+global using System.Text;
