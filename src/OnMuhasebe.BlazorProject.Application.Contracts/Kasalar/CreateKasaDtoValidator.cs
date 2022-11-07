b8f348181018e3d977e3cc3725c070c23292a421
dogustuluk <dogus.tuluk@gmail.com>
2022-11-07 14:35:07 +0300

    Donem, Hizmet, Kasa and Masraf validators

---
+++ src/OnMuhasebe.BlazorProject.Application.Contracts/Kasalar/CreateKasaDtoValidator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnMuhasebe.BlazorProject.Kasalar;
+public class CreateKasaDtoValidator : AbstractValidator<CreateKasaDto>
+{
+    public CreateKasaDtoValidator(IStringLocalizer<BlazorProjectResource> localizer)
+    {
+        RuleFor(x => x.Kod)
+           .NotEmpty()
+           .WithMessage(localizer[BlazorProjectDomainErrorCodes.Required, localizer["Code"]])
+
+           .MaximumLength(EntityConsts.MaxKodLength)
+           .WithMessage(localizer[BlazorProjectDomainErrorCodes.MaxLenght, localizer["Code"], EntityConsts.MaxKodLength]);
+
+        RuleFor(x => x.Ad)
+            .NotEmpty()
+            .WithMessage(localizer[BlazorProjectDomainErrorCodes.Required, localizer["Name"]])
+
+            .MaximumLength(EntityConsts.MaxAdLength)
+            .WithMessage(localizer[BlazorProjectDomainErrorCodes.MaxLenght, localizer["Name"], EntityConsts.MaxAdLength]);
+
+        RuleFor(x => x.SubeId)
+            .Must(x => x.HasValue && x.Value != Guid.Empty)
+            .WithMessage(localizer[BlazorProjectDomainErrorCodes.Required, localizer["Branch"]]);
+
+        RuleFor(x => x.Aciklama)
+            .MaximumLength(EntityConsts.MaxAciklamaLength)
+            .WithMessage(localizer[BlazorProjectDomainErrorCodes.MaxLenght, localizer["Description"], EntityConsts.MaxAciklamaLength]);
+    }
+}
--- src/OnMuhasebe.BlazorProject.Domain.Shared/BlazorProjectDomainErrorCodes.cs
+++ src/OnMuhasebe.BlazorProject.Domain.Shared/BlazorProjectDomainErrorCodes.cs
@@ -10,4 +10,5 @@ public static class BlazorProjectDomainErrorCodes
     public const string CannotBeDeleted = "Exception:00002";
     public const string Required = "Exception:00003";
     public const string MaxLenght = "Exception:00004";
+    public const string GreaterThanOrEqual = "Exception:00005";
 }
