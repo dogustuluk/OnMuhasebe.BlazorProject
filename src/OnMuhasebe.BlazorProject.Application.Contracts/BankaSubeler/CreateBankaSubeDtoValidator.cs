76d7106c8458012e667d50d2b9519b1f40464be1
dogustuluk <dogus.tuluk@gmail.com>
2022-11-07 14:00:32 +0300

    BankaSube, Birim, Cari and Depo validators

---
+++ src/OnMuhasebe.BlazorProject.Application.Contracts/BankaSubeler/CreateBankaSubeDtoValidator.cs
@@ -0,0 +1,30 @@
+﻿namespace OnMuhasebe.BlazorProject.BankaSubeler;
+public class CreateBankaSubeDtoValidator : AbstractValidator<CreateBankaSubeDto>
+{
+    public CreateBankaSubeDtoValidator(IStringLocalizer<BlazorProjectResource> localizer)
+    {
+        RuleFor(x => x.Kod)
+           .NotEmpty()
+           .WithMessage(localizer[BlazorProjectDomainErrorCodes.Required, localizer["Code"]])
+
+           .MaximumLength(EntityConsts.MaxKodLength)
+           .WithMessage(localizer[BlazorProjectDomainErrorCodes.MaxLenght, localizer["Code"], EntityConsts.MaxKodLength]);
+
+        RuleFor(x => x.Ad)
+            .NotEmpty()
+            .WithMessage(localizer[BlazorProjectDomainErrorCodes.Required, localizer["Name"]])
+
+            .MaximumLength(EntityConsts.MaxAdLength)
+            .WithMessage(localizer[BlazorProjectDomainErrorCodes.MaxLenght, localizer["Name"], EntityConsts.MaxAdLength]);
+
+        RuleFor(x => x.BankaId)
+            .Must(x => x.HasValue && x.Value != Guid.Empty)
+            .WithMessage(localizer[BlazorProjectDomainErrorCodes.Required, localizer["Bank"]]);
+
+        RuleFor(x => x.Aciklama)
+            .MaximumLength(EntityConsts.MaxAciklamaLength)
+            .WithMessage(localizer[BlazorProjectDomainErrorCodes.MaxLenght, localizer["Description"], EntityConsts.MaxAciklamaLength]);
+        
+
+    }
+}
